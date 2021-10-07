1236f65bdb05ec124c9ed0155908d3874c2b1fca
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-10-07 16:15:38 +0200

    mod

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 alert("crea la tua password")
+
 const nomeUser = prompt("Inserisci il tuo nome.")
-console.log("nome" ,nomeUser )
+console.log("nome", nomeUser)
 
 const cognomeUser = prompt("Inserisci il tuo cognome.")
-console.log("cognome" ,cognomeUser )
+console.log("cognome", cognomeUser)
 
 const coloreUser = prompt("Inserisci il tuo colore.")
-console.log("colore preferito" ,coloreUser )
+console.log("colore preferito", coloreUser)
 
-alert("password: " + nomeUser + cognomeUser + coloreUser + 21)
+alert("password: "+ nomeUser + cognomeUser + coloreUser + 21)
