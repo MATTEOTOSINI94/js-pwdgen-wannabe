bf9301dea769761daf556c79a64e3eca4ac5194b
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-10-07 14:54:48 +0200

    mod

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const nomeUser = prompt("Inserisci il tuo nome.")
+console.log("nome" ,nomeUser )
+
+const cognomeUser = prompt("Inserisci il tuo cognome.")
+console.log("cognome" ,cognomeUser )
+
+const coloreUser = prompt("Inserisci il tuo colore.")
+console.log("colore preferito" ,coloreUser )
+
+alert(nomeUser + cognomeUser + coloreUser + 21)
